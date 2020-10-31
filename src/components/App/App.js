50e83658c52675973fed9557234fa301dafd510a
Kelly Danger <kellyanndanger@gmail.com>
2020-10-31 08:28:08 -0500

    feelings router setup and working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {connect} from 'react-redux';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+//styling
 import './App.css';
+
+//components
+import FeelingsForm from '../FeelingsForm/FeelingsForm';
+
 
 class App extends Component {
   render() {
@@ -11,6 +18,10 @@ class App extends Component {
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+        <Router>
+          <Route exact path = "/feelings" component={FeelingsForm}/>
+        </Router>
+       
       </div>
     );
   }
---
+++ src/components/FeelingsForm/FeelingsForm.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class FeelingsForm extends Component {
+  render(){
+    return(
+      <p>in feelingsForm</p>
+    )
+  }
+}
+
+
+
+export default FeelingsForm;
