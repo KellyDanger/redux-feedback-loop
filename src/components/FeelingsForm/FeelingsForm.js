8660a6f4df276160f0a15c1cb6ec24ac3f58f6d3
Kelly Danger <kellyanndanger@gmail.com>
2020-10-31 09:21:36 -0500

    button and input connected to functions

--- src/components/FeelingsForm/FeelingsForm.js
+++ src/components/FeelingsForm/FeelingsForm.js
@@ -3,9 +3,26 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class FeelingsForm extends Component {
+state = {
+  feeling: 0
+}
+
+handleFeelingChange = () => {
+  console.log('Feelings!!!');
+  
+}
+
+handleSubmit = () => {
+  console.log('clicked feelings');
+}
+
   render(){
     return(
-      <p>in feelingsForm</p>
+      <form onSubmit={this.handleSubmit}>
+        <lable htmlFor="feelingsInput">Feeling?</lable>
+        <input type="number" onChange={this.handleFeelingChange}/>
+        <button onClick={this.handleSubmit}>Next</button>
+      </form>
     )
   }
 }
