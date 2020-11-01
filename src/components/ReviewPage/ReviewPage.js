de78a0f23803c8e319e55673691831d6089997ab
Kelly Danger <kellyanndanger@gmail.com>
2020-10-31 19:16:15 -0500

    review submit button added and clicking

--- src/components/ReviewPage/ReviewPage.js
+++ src/components/ReviewPage/ReviewPage.js
@@ -4,16 +4,22 @@ import {connect} from 'react-redux';
 
 class ReviewPage extends Component {
 
+  handleClick = () => {
+    console.log('clicked');
+    
+  }
+
   render(){
     console.log('REDUX STATE', this.props.reduxState);
     
     return(
       <ul>
-        {/* {JSON.stringify(this.props.reduxState)} */}
+        <h1>Review Your Feedback</h1>
         <li>Feelings: {this.props.reduxState.feelingsReducer.feelings}</li>
         <li>Understanding: {this.props.reduxState.understandingReducer.understanding}</li>
         <li>Support: {this.props.reduxState.supportReducer.support}</li>
         <li>Comment: {this.props.reduxState.commentReducer.comment}</li>
+        <button onClick={this.handleClick}>SUBMIT</button>
       </ul>
     )
   }
